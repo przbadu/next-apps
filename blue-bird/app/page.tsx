a934f5c74593e98278237df2a1e781885400cca9
przbadu <pushparaj.badu@gmail.com>
2023-10-03 10:58:02 +0545

    Redirect to /sign_in or / page based on authentication state of the user

--- blue-bird/app/page.tsx
+++ blue-bird/app/page.tsx
@@ -1,10 +1,19 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from 'next/headers'
+import { redirect } from "next/navigation"
 import AuthButtonServer from "./components/auth-button-server"
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
 
+  // redirect to login if not logged in
+  const { data: { session } } = await supabase.auth.getSession()
+
+  if (!session) {
+    redirect('/login')
+  }
+
+  // authenticated code
   const { data: tweets } = await supabase.from("tweets").select()
 
   return (
