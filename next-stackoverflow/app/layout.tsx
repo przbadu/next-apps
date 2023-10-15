b7021fddd313f87550ed5895467f3970c2920c51
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-10-15 21:40:19 +0545

    Add assets and layout

--- next-stackoverflow/app/layout.tsx
+++ next-stackoverflow/app/layout.tsx
@@ -1,15 +1,30 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description: "DevFlow - Where Developers Learn, Share and Build Carrer",
+  title: "DevFlow- Where Developers Learn, Share and Build Carrer",
+  description:
+    "A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -18,9 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-graident hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is a heading text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
